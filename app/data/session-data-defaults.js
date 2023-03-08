c21b04724c2e659270f72fa0d1aec5b5517d78f7
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2023-03-08 11:58:41 +0000

    Update to qual service

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -7,7 +7,6 @@ module.exports = {
   lastName: 'Smith',
   email: 'Ja******@gmail.com',
   phone: '078********33',
-  tsa: 'Teaching services account',
   dia: 'DfE Identity account',
   dob: { day: 2, month: 12, year: 1989 },
   dqtDob: { day: 2, month: 12, year: 1989 }
